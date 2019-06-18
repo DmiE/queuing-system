b08670e1f2a96a7f79a7a978483bba8905826a93
Dawid <Dawid.Mieszczak95@gmail.com>
2019-06-18 12:42:54 -0400

    add register functionality to frontend

---
+++ frontend/mgr-frontend/src/containers/Register/Register.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import './Register.css';
+import axios from 'axios';
+import Input from '../../components/input/Input';
+
+class Register extends Component {
+    state = {
+        inputs: {
+            firstName: {
+                elementConfig: {
+                    type: 'text',
+                    name: 'firstName',
+                    placeholder: 'First Name'
+                },
+                value: ''
+            },
+            lastName: {
+                elementConfig: {
+                    type: 'text',
+                    name: 'lastName',
+                    placeholder: 'Last Name'
+                },
+                value: ''
+            },
+            eMail: {
+                elementConfig: {
+                    type: 'email',
+                    name: 'email',
+                    placeholder: 'E-mail Address'
+                },
+                value: ''
+            },
+            password: {
+                elementConfig: {
+                    type: 'text',
+                    name: 'password',
+                    placeholder: 'Password'
+                },
+                value: ''
+            }
+        }
+    }
+
+    registerHandler = ( event ) => {
+        event.preventDefault();
+        const formData = {
+            email: this.state.inputs.eMail.value,
+            firstName: this.state.inputs.firstName.value,
+            lastName: this.state.inputs.lastName.value,
+            password: this.state.inputs.password.value
+        }
+        // axios.post('https://smoothienewsletter.firebaseio.com/newsletters.json', formData)
+        axios.post('http://192.168.0.25:5000/api/auth/signup', formData)
+            .then( response => {
+                console.log(response + "form was send")
+        })
+    }
+
+    inputHandler = (event, inputId) => {
+        const updatedStateInputs = { ...this.state.inputs };
+        const updatedStateInputsElement = { ...updatedStateInputs[inputId] }
+
+        updatedStateInputsElement.value = event.target.value;
+        updatedStateInputs[inputId] = updatedStateInputsElement;
+
+        this.setState({ inputs: updatedStateInputs });
+    }
+
+    render() {
+        const inputArray = [];
+        for (let key in this.state.inputs) {
+            inputArray.push({
+                id: key,
+                config: this.state.inputs[key]
+            })
+        }
+
+
+        return (
+            <div>
+                <form onSubmit={this.registerHandler}>
+                    {inputArray.map(inputObject => (
+                        <Input key={inputObject.id} elementConfig={inputObject.config.elementConfig} value={inputObject.config.value} changed={(event) => this.inputHandler(event, inputObject.id)} />
+                    ))}
+                    <button type="submit">Register In</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default Register;
