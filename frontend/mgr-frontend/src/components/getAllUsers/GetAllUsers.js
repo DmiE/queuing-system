c131166bcfb59091a8fd8146baca34b2d30c1075
Dawid <Dawid.Mieszczak95@gmail.com>
2019-06-20 16:45:37 -0400

    add redux to project and create global state

---
+++ frontend/mgr-frontend/src/components/getAllUsers/GetAllUsers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const getAllUsers = () => {
+
+}
+
+
+// getAllUsers = () => {
+//     axios.get('http://192.168.0.25:5000/api/user/getAll', { headers: { Authorization: this.props.authorizationToken } })
+//         .then((response) => {
+//             const users = response.data.users
+//             console.log(users)
+//         }
+//         )
+// }
+
+export default getAllUsers;
---
+++ frontend/mgr-frontend/src/containers/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+    render () {
+        return (
+            <section>
+                
+            </section>
+        )
+    }
+}
+
+export default Main
